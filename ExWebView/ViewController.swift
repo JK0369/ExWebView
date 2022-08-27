ae91c4b4d38914049b238ae9560b1e8ee1297d81
jake <palatable7@naver.com>
2022-08-27 17:23:53 +0900

    Implement

--- ExWebView/ViewController.swift
+++ ExWebView/ViewController.swift
@@ -6,14 +6,57 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    WKWebsiteDataStore.default()
+      .fetchDataRecords(
+        ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()
+      ) { records in
+        records
+          .forEach {
+            WKWebsiteDataStore.default()
+              .removeData(
+                ofTypes: $0.dataTypes,
+                for: [$0],
+                completionHandler: {}
+              )
+          }
+      }
+    
+    WKWebViewConfiguration.includeCookie(
+      cookies: [.init()],
+      completion: { configuration in
+        guard let config = configuration else { return }
+        let webView = WKWebView(frame: .zero, configuration: config)
+        print("쿠키가 세팅된 webView 완성", webView)
+      })
   }
-
-
 }
 
+extension WKWebViewConfiguration {
+  static func includeCookie(cookies: [HTTPCookie], completion: @escaping (WKWebViewConfiguration?) -> Void) {
+    let config = WKWebViewConfiguration()
+    let dataStore = WKWebsiteDataStore.nonPersistent()
+    
+    DispatchQueue.main.async {
+      let waitGroup = DispatchGroup()
+      
+      for cookie in cookies {
+        waitGroup.enter()
+        dataStore.httpCookieStore.setCookie(cookie) {
+          waitGroup.leave()
+        }
+      }
+      
+      waitGroup.notify(queue: DispatchQueue.main) {
+        config.websiteDataStore = dataStore
+        completion(config)
+      }
+    }
+  }
+}
